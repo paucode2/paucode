8ba58d620fb5dfd3e7d39c4c1aa931ed7f69d2db
paucode1 <choquepaucarj.social@gmail.com>
2023-07-19 20:09:43 -0500

    correción de la función va

--- paucode/eco.py
+++ paucode/eco.py
@@ -1,2 +1,13 @@
 def va(i, nper, pago, vf):
-    return vf*(1+i)**nper
+    """Valor actual o valor presente. Se utiliza para actualizar el valor final a una tasa i% en nper periodos de tiempo
+
+    Args:
+        i (_type_): Tasa de interes en porcentajes
+        nper (_type_): numero de periodos para actualizar
+        pago (_type_): pagos periodicos
+        vf (_type_): Valor final
+
+    Returns:
+        _type_: Valor actual o actualización al momento cero
+    """
+    return vf/(1+i)**nper
